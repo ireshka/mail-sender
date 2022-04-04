fe3fa01d59d46eccff3f4b3bb56b5a482944cc9e
ireshka <ireshka@users.noreply.github.com>
2022-04-04 23:56:48 +0200

    feat: add basic styling for <Rating /> page

--- pages/rating.tsx
+++ pages/rating.tsx
@@ -9,11 +9,15 @@ import {
   TextField,
   Typography,
   Alert,
+  CardHeader,
+  Grid,
 } from "@mui/material";
 import { useRouter } from "next/router";
 import { GetServerSideProps, InferGetServerSidePropsType } from "next";
 import { ParsedUrlQuery } from "querystring";
 import { SendMail } from "../api/sendMail";
+import { styled } from "@mui/system";
+import Image from "next/image";
 
 type ExpectedQuery = {
   mail: string;
@@ -64,7 +68,7 @@ const Rating = ({
           mail: queryObject.mail,
           rating: queryObject.rating,
         });
-        console.log('Response from backend:')
+        console.log("Response from backend:");
         console.log(data);
       }
     };
@@ -72,22 +76,65 @@ const Rating = ({
     sendRating();
   });
 
+  const WrapperBox = styled(Box)`
+    padding: 16px;
+  `;
+
   return (
     <Container maxWidth="md">
-      <Card
+      <Paper
         sx={{
-          padding: "20px",
+          padding: "0",
           marginTop: "20px",
         }}
       >
-        {rating !== null ? (
-          <Box>Thank for your rating! You gave us: {rating} stars.</Box>
-        ) : (
-          <Alert severity="error">
-            The old gnomes ate your answer. Please rate us again.
-          </Alert>
-        )}
-      </Card>
+        <Box
+          sx={{
+            backgroundColor: "#1976d2",
+            borderTopLeftRadius: "8px",
+            borderTopRightRadius: "8px",
+          }}
+        >
+          <WrapperBox>
+            <Typography
+              variant="h2"
+              component="h1"
+              color="common.white"
+              align="center"
+            >
+              El Trio Intern Project
+            </Typography>
+          </WrapperBox>
+        </Box>
+
+        <WrapperBox>
+          <Grid container spacing={2} sx={{ pt: 4, pb: 4 }}>
+            <Grid item xs={12} sm={6}>
+              <Image
+                src="/complete.png"
+                alt=""
+                width={704}
+                height={844}
+                layout="responsive"
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              {rating !== null ? (
+                <Box>
+                  <Typography variant="h5" sx={{pb: 5}}>Thank for your rating!</Typography>
+                  <Typography variant="h6">
+                    You gave us: <strong>{rating}</strong> stars.
+                  </Typography>
+                </Box>
+              ) : (
+                <Alert severity="error">
+                  The old gnomes ate your answer. Please rate us again.
+                </Alert>
+              )}
+            </Grid>
+          </Grid>
+        </WrapperBox>
+      </Paper>
     </Container>
   );
 };
