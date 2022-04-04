c5306583ec4100994d33d557d5169184ddb2b860
KlaudiaDabrowska <klaudia@DESKTOP-U5H299B.localdomain>
2022-04-04 10:54:03 +0200

    feat: Add formik and validation email

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,31 @@
-import { Box, Button, Container, FormControl, Paper, Stack, TextField, Typography } from '@mui/material'
-import type { NextPage } from 'next'
+import {
+  Box,
+  Button,
+  Container,
+  FormControl,
+  Paper,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
+import type { NextPage } from "next";
+import { useFormik } from "formik";
+import * as yup from "yup"
 
 const Home: NextPage = () => {
+  const validationSchema = yup.object({
+    email: yup.string().email("Enter a valid email").required("Email is required")
+  })
+
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+    },
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
   return (
     <Container maxWidth="sm">
       <Paper
@@ -19,22 +43,40 @@ const Home: NextPage = () => {
           overflowY: "auto",
         }}
       >
-        <Box sx={{marginBottom: '2rem'}}>
-          <Typography variant="h4" component="h1">Get your survey</Typography>
-          <Typography variant="subtitle1">Give us your mail and you can give us stars</Typography>
+        <Box sx={{ marginBottom: "2rem" }}>
+          <Typography variant="h4" component="h1">
+            Get your survey
+          </Typography>
+          <Typography variant="subtitle1">
+            Give us your mail and you can give us stars
+          </Typography>
         </Box>
-        <FormControl component="form" autoComplete="off" sx={{ width: "100%" }}>
+        <FormControl
+          component="form"
+          autoComplete="off"
+          sx={{ width: "100%" }}
+          onSubmit={formik.handleSubmit}
+        >
           <TextField
             fullWidth
             placeholder="Your email"
             size="small"
             sx={{ marginBottom: "1rem" }}
+            id="email"
+            name="email"
+            type="email"
+            value={formik.values.email}
+            onChange={formik.handleChange}
+            error={formik.touched.email && Boolean(formik.errors.email)}
+            helperText={formik.touched.email && formik.errors.email}
           />
-          <Button variant="outlined">Get you survey</Button>
+          <Button variant="outlined" type="submit">
+            Get you survey
+          </Button>
         </FormControl>
       </Paper>
     </Container>
   );
-}
+};
 
-export default Home
+export default Home;
