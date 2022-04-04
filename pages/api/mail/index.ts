bc930dbb98f07149452b1b52ee169f80fcd5d1f0
mwakulinski <michal.wakulinski1@gmail.com>
2022-04-04 15:02:45 +0200

    add nodemail implementation

--- pages/api/mail/index.ts
+++ pages/api/mail/index.ts
@@ -1,45 +1,20 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import { sendMessageToUser } from "../../../helpers/sendEmail";
 import { IUserMailRequest, IUserMailResponse } from "../../../types/Mail";
+
 const sgMail = require("@sendgrid/mail");
 const SendGrid_Key = process.env.SENDGRID_KEY;
-import { render } from "mjml-react";
-import * as ratingEmail from "../../../emailTemplates/ratingEmail";
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<IUserMailResponse>
 ) {
   const body: IUserMailRequest = req.body.mail;
-
-  sgMail.setApiKey(SendGrid_Key);
-
-  //sprawdzić mail
-
-  const { html: emailHtml } = render(ratingEmail.generate(), { validationLevel: 'soft' })
-
-  const msg = {
-    to: `${body}`,
-    from: "michal.wakulinski1@gmail.com", // Use the email address or domain you verified above
-    subject: "Intern Survey",
-    text: "and easy to do anywhere, even with Node.js",
-    html: emailHtml,
-  };
-
-  //ES8
-  (async () => {
-    try {
-      await sgMail.send(msg);
-      res
-        .status(200)
-        .json({ responseMessage: `mail has been sent to ${body}` });
-    } catch (error: any) {
-      // console.error(error);
-      console.log('===> im in catch?')
-
-      if (error.response) {
-        console.error(error.response.body);
-      }
-      res.status(404).json({ errorMessage: `Sorry something went wrong` });
-    }
-  })();
+  try {
+    await sendMessageToUser(req.body.mail);
+    res.status(200).json({ responseMessage: `mail has been sent to ${body}` });
+  } catch (error) {
+    console.log(JSON.stringify(error));
+    res.status(404).json({ errorMessage: `Sorry something went wrong` });
+  }
 }
