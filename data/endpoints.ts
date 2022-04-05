f3fc1c42733b83e5e92a1a7f681a52c5f8c81e25
ireshka <ireshka@users.noreply.github.com>
2022-04-06 01:04:31 +0200

    fix(cr): get base url from env & change endpoints object

---
+++ data/endpoints.ts
@@ -0,0 +1,4 @@
+export const endpoints = {
+  SEND: `${process.env.NEXT_PUBLIC_BASE_URL}/api/mail`,
+  RATING: `${process.env.NEXT_PUBLIC_BASE_URL}/api/rating`,
+};
--- types/Endpoints.enum.ts
+++
@@ -1,4 +0,0 @@
-export enum Endpoints {
-  SEND = "http://localhost:3000/api/mail",
-  RATING = "http://localhost:3000/api/rating",
-}
